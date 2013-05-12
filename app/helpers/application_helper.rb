114da36fd0086e5d217d9e97f96b1960e738c020
andy <andy@baoleihang.com>
2013-05-12 12:23:15 +0800

    修改 logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,7 +68,7 @@ module ApplicationHelper
   end
 
   def render_page_title
-    site_name = Setting.app_name
+    site_name = t("web_title")
     title = @page_title ? "#{site_name} | #{@page_title}" : site_name rescue "SITE_NAME"
     content_tag("title", title, nil, false)
   end
